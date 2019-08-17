b2b704b0792bda9ff75bce368a1f76adbe3bdffa
svetlana <manichevassvetlana@gmail.com>
2019-08-17 11:03:11 +0300

    Initial commit

---
+++ app/Http/Controllers/HelpControllers/ResourceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\HelpControllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ResourceController extends Controller
+{
+    //
+}
